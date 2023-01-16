4587513128aaaf3b73222a987b2f9aab193e06ab
snanbyndr67 <snan677@yandex.com>
2023-01-16 20:29:52 +0300

    Initial commit

---
+++ src/main/java/com/quizapp/multiplechoice/MultipleChoiceApplication.java
@@ -0,0 +1,14 @@
+package com.quizapp.multiplechoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleChoiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MultipleChoiceApplication.class, args);
+	}
+
+}
